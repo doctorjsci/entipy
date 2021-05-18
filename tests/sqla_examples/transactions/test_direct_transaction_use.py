59ae8216719eb17a6fd32fbda3d35dc21f5f0f32
pythonclassic <clearsoft.studio@gmail.com>
2021-05-18 17:08:01 +0000

    message

---
+++ tests/sqla_examples/transactions/test_direct_transaction_use.py
@@ -0,0 +1,26 @@
+import sqlalchemy as db
+from sqlalchemy import create_engine, MetaData
+from sqla_examples.transactions.direct_transaction_use import direct_transaction_use
+
+engine = create_engine('sqlite:///direct_transaction_use.db')
+metadata = MetaData()
+
+libs = db.Table(
+    'libs', metadata,
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('name', db.String(50), nullable=False),
+    db.Column('description', db.String(500), nullable=False)
+)
+
+def test_direct_transaction_use():
+  	metadata.create_all(bind=engine)
+    
+	direct_transaction_use()
+
+	with engine.connect() as connection:
+		with connection.begin():
+			res = connection.execute(libs.select())
+			first_row = res.fetchone()
+			assert first_row == (1, 'SqlAlchemy', 'The Database Toolkit for Python')
+
+	metadata.drop_all(bind=engine)
