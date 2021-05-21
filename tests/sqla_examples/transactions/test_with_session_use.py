540e98fe06a945eb41294217ef1e5e64a9fb8f60
pythonclassic <clearsoft.studio@gmail.com>
2021-05-21 14:58:18 +0000

    message

---
+++ tests/sqla_examples/transactions/test_with_session_use.py
@@ -0,0 +1,12 @@
+from sqla_examples.transactions.with_session_use import Base, Session, engine, with_session_use
+
+def test_with_session_use():
+    Base.metadata.create_all(bind=engine)
+    try:
+        with_session_use('Flask', '1.1.2')
+
+        session = Session()
+        assert session.query(Framework).first().name == 'Flask'
+        session.close()
+    finally:
+        Base.metadata.drop_all(bind=engine)
